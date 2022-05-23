75301079a69b442067fd1ce58addd53484be1170
camilogv71 <camilogv71@gmail.com>
2022-05-22 21:32:03 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 22 18:18:45 2022
+Created on Sat May 21 16:18:45 2022
 
-@author: Juan Camilo González Vélez
+@author: 50315
 """
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
- 
+
 setuptools.setup(
     name="GD_Package",
     version="0.0.1",
@@ -16,7 +16,6 @@ setuptools.setup(
     author_email="camilogv71@gmail.com",
     description="Gradient Descent Algorithm Package",
     long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/camilogv71/GD_Package",
     project_urls={
         "Bug Tracker": "https://github.com/camilogv71/GD_Package/issues",
@@ -26,7 +25,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "GD_Package"},
     packages=["gdtest"],
     python_requires=">=3.8",
 )
